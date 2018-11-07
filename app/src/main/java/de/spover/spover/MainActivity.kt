d7b256c0a5a94ef6c824ffb8dabfe6f1c20e830e
MaX-Lo <lorenz_max@gmx.de>
2018-11-07 15:06:46 +0100

    added overlay example

--- app/src/main/java/de/spover/spover/MainActivity.kt
+++ app/src/main/java/de/spover/spover/MainActivity.kt
@@ -1,12 +1,69 @@
 package de.spover.spover
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (Settings.canDrawOverlays(this)) {
+
+            // Launch service right away - the user has already previously granted permission
+            launchMainService()
+        } else {
+
+            // Check that the user has granted permission, and prompt them if not
+            checkDrawOverlayPermission()
+        }
+    }
+
+    private fun launchMainService() {
+
+        val svc = Intent(this, OverlayService::class.java)
+
+        startService(svc)
+
+        finish()
+    }
+
+    fun checkDrawOverlayPermission() {
+
+        // Checks if app already has permission to draw overlays
+        if (!Settings.canDrawOverlays(this)) {
+
+            // If not, form up an Intent to launch the permission request
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+
+            // Launch Intent, with the supplied request code
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        // Check if a request code is received that matches that which we provided for the overlay draw request
+        if (requestCode == REQUEST_CODE) {
+
+            // Double-check that the user granted it, and didn't just dismiss the request
+            if (Settings.canDrawOverlays(this)) {
+
+                // Launch the service
+                launchMainService()
+            } else {
+
+                Toast.makeText(this, "Sorry. Can't draw overlays without permission...", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+
+        const val REQUEST_CODE = 10101
     }
 }
