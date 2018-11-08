ff67557abaa674b888a8141c1d28606aa6dfa0e9
MaX-Lo <lorenz_max@gmx.de>
2018-11-08 14:50:22 +0100

    view isn't jumping around like crazy when dragging anymore, keyboard and gestures work again while having the overlay open

--- app/src/main/java/de/spover/spover/OverlayService.kt
+++ app/src/main/java/de/spover/spover/OverlayService.kt
@@ -6,9 +6,7 @@ import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
 import android.support.v4.view.GestureDetectorCompat
-import android.util.Log
 import android.view.*
-import android.widget.FrameLayout
 
 class OverlayService: Service(), View.OnTouchListener {
 
@@ -34,91 +32,51 @@ class OverlayService: Service(), View.OnTouchListener {
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT)
 
-        params!!.gravity = Gravity.CENTER_VERTICAL or Gravity.RIGHT// or Gravity.START
+        params!!.gravity = Gravity.TOP or Gravity.START// or Gravity.START
         params!!.x = 300
         params!!.y = 0
 
-        val interceptorLayout = object : FrameLayout(this) {
 
-            override fun dispatchKeyEvent(event: KeyEvent): Boolean {
-
-                // Only fire on the ACTION_DOWN event, or you'll get two events (one for _DOWN, one for _UP)
-                if (event.action == KeyEvent.ACTION_DOWN) {
-
-                    // Check if the HOME button is pressed
-                    if (event.keyCode == KeyEvent.KEYCODE_BACK) {
-
-                        Log.v(TAG, "BACK Button Pressed")
-
-                        // As we've taken action, we'll return true to prevent other apps from consuming the event as well
-                        return true
-                    }
-                }
-
-                // Otherwise don't intercept the event
-                return super.dispatchKeyEvent(event)
-            }
-        }
-
-        floatingView = (getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.floating_view, interceptorLayout)
+        floatingView = (getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.floating_view, null)
         floatingView!!.setOnTouchListener(this)
         windowManager!!.addView(floatingView, params)
-
-        mDetector = GestureDetectorCompat(this, MyGestureListener(this))
     }
 
     override fun onDestroy() {
-
         super.onDestroy()
-
         if (floatingView != null) {
             windowManager!!.removeView(floatingView)
             floatingView = null
         }
     }
 
+    private var touchStartX = 0f
+    private var touchStartY = 0f
+    private var viewStartX = 0
+    private var viewStartY = 0
     override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
-        return mDetector.onTouchEvent(motionEvent)
-    }
+        if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+            touchStartX = motionEvent.rawX
+            touchStartY = motionEvent.rawY
 
-    private class MyGestureListener(var overlayService: OverlayService) : GestureDetector.OnGestureListener {
+            viewStartX = params!!.x
+            viewStartY = params!!.y
+        } else if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+            val dX = motionEvent.rawX - touchStartX
+            val dY = motionEvent.rawY - touchStartY
 
-        override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-            // not needed
-            return false
+            params!!.x = (dX + viewStartX).toInt()
+            params!!.y = (dY + viewStartY).toInt()
+
+            try {
+                windowManager!!.updateViewLayout(floatingView, params)
+            } catch (ignore: IllegalArgumentException) {
+                // FixMe
+            }
         }
-
-        override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, distX: Float, distY: Float): Boolean {
-            val params = overlayService.params
-            params!!.x += distX.toInt()
-            params!!.y -= distY.toInt()
-            overlayService.windowManager!!.updateViewLayout(overlayService.floatingView, params)
-            return true
-        }
-
-        override fun onLongPress(p0: MotionEvent?) {
-            // not needed
-        }
-
-        override fun onShowPress(p0: MotionEvent?) {
-            // not needed
-        }
-
-        override fun onDown(event: MotionEvent): Boolean {
-            // not needed
-            return false
-        }
-
-        override fun onFling(e1: MotionEvent, e2: MotionEvent, velX: Float, velY: Float): Boolean {
-            // not needed
-            return false
-        }
-    }
-
-    companion object {
-        private var TAG = OverlayService::class.java.simpleName
+        return true
     }
 }
