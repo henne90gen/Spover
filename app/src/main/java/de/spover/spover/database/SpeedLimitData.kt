017f43e60a44f724233c0910a6b54221cca2e54a
Henne <henne90gen@gmail.com>
2018-12-03 14:55:17 +0100

    Adding room persistence library

--- app/src/main/java/de/spover/spover/database/SpeedLimitData.kt
+++ app/src/main/java/de/spover/spover/database/SpeedLimitData.kt
@@ -1,39 +1,37 @@
-//package de.spover.spover.database
-//
-//import androidx.room.*
-//
-//@Entity
-//data class User(
-//        @PrimaryKey var id: Int,
-//        @ColumnInfo(name = "first_name") var firstName: String?,
-//        @ColumnInfo(name = "last_name") var lastName: String?
-//)
-//
-//@Dao
-//interface UserDao {
-//    @Query("SELECT * FROM user")
-//    fun getAll(): List<User>
-//
-//    @Query("SELECT * FROM user WHERE id IN (:userIds)")
-//    fun loadAllByIds(userIds: IntArray): List<User>
-//
-//    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-//            "last_name LIKE :last LIMIT 1")
-//    fun findByName(first: String, last: String): User
-//
-//    @Insert
-//    fun insertAll(vararg users: User)
-//
-//    @Delete
-//    fun delete(user: User)
-//}
-//
-//@Database(entities = arrayOf(User::class), version = 1)
-//abstract class AppDatabase : RoomDatabase() {
-//    abstract fun userDao(): UserDao
-//}
-//
-//val db = Room.databaseBuilder(
-//        this,
-//        AppDatabase::class.java, "database-name"
-//).build()
+package de.spover.spover.database
+
+import androidx.room.*
+
+@Entity
+data class SpeedLimit(
+
+        @PrimaryKey
+        var id: Int,
+
+        var latitude: Double,
+        var longitude: Double,
+
+        val allowedSpeed: Int
+)
+
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM speedlimit")
+    fun getAll(): List<SpeedLimit>
+
+    @Query("SELECT * FROM speedlimit WHERE id IN (:speedLimitIds)")
+    fun loadAllByIds(speedLimitIds: IntArray): List<SpeedLimit>
+
+    @Insert
+    fun insertAll(vararg speedlimits: SpeedLimit)
+
+    @Delete
+    fun delete(user: SpeedLimit)
+}
+
+@Database(entities = arrayOf(SpeedLimit::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+}
